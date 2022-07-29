3a86cd4b73305735877eab922ce708c5837f9c47
Adam Burski <burskiadamwork@gmail.com>
2022-07-29 20:08:56 +0200

    fisnish login page style

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
+<script>
+        
+        let lang = document.cookie;
+        
+        if(lang.search("lang") == -1){
+            const userLocale = navigator.languages && navigator.languages.lengh ? navigator.languages[0] : navigator.language;
+            document.cookie = "lang="+userLocale+";expires=Fri, 31 Dec 9999 23:59:59 GMT";
+            console.log('Create new'+userLocale);
+            if(lang.substr(lang.search("lang")+5,2) == 'en'){
+                location.href = "./login/en-US";
+            }else{
+                location.href = "./login/pl-PL";
+            }
+        }else{
+            if(lang.substr(lang.search("lang")+5,2) == 'en'){
+                location.href = "./login/en-US";
+            }else{
+                location.href = "./login/pl-PL";
+            }
+        }
 
-header("Location: login/")
-
-?>
+</script>
--- login/en-US/index.php
+++ login/en-US/index.php
@@ -8,10 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Working Time Schedule</title>
     <script src="/../scripts/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style/clear_css.css">
-    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="../style/clear_css.css">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
     <script src="/../scripts/loadingPage.js"></script>
-    <link rel="icon" type="image/x-icon" href="style/img/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../style/img/favicon.ico">
 </head>
 <body>
     <iframe src="loading.html" id="loading" style="position: absolute; width: 100vw; height: 100vh; margin:0;"></iframe>
@@ -50,6 +50,39 @@
             </div>
         </div>
         <div class="rightPanelLogin">
+            <div class="changeLanguage">
+                <div class="pl-lang" onclick="document.location = '../pl-PL/'">PL</div>
+                |
+                <div class="eng-lang" style="color: #0A85ED;" onclick="document.location = '../en-US/'">ENG</div>
+            </div>
+            <div class="helloText">
+                <h2>Hello Again!</h2>
+                <p>Log in to your account</p>
+            </div>
+            <div class="login-form">
+                <form class="form-1">
+                    <div class="ulog fontform">LOGIN</div>
+                    <input type="text" name="usrlogin" id="usrlogin" placeholder="LOGIN OR EMAIL"/>
+                    <div class="upass fontform">PASSWORD</div>
+                    <div class="passwo">
+                        <input type="password" id="usrpass" name="usrpass" placeholder="      PASSWORD"/>
+                        <div id="showp" class="showPass" onmousedown="showpass()" onmouseup="hidepass()"></div>
+                    </div>
+                    <p><a href="#forgotpass">Forgot password?</a></p>
+                    <input class="usub btn-1" type="submit" value="Log in"/>
+                </form>       
+            </div>
+            <script>
+                function showpass(){
+                    document.getElementById('showp').style.backgroundImage = 'url("../style/img/Icon6.png")';
+                    document.getElementById('usrpass').type = 'text';
+                }
+                function hidepass(){
+                    document.getElementById('showp').style.backgroundImage = 'url("../style/img/Icon5.png")';
+                    document.getElementById('usrpass').type = 'password';
+                }
+            </script>
+            <div class="copyright">© 2022 | PIESIOSPACE CORPORATION. ALL RIGHTS RESERVED</div>
         </div>
     </div>
 </body>
