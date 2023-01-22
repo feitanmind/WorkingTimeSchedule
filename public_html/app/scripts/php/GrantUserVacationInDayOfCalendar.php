00d17cfcf5c720bb417cca316ff3125d565c0a3b
Adam Burski <burskiadamwork@gmail.com>
2023-01-22 10:03:06 +0100

    repair error php header location replace window hostory push state js

--- public_html/app/scripts/php/GrantUserVacationInDayOfCalendar.php
+++ public_html/app/scripts/php/GrantUserVacationInDayOfCalendar.php
@@ -63,9 +63,10 @@ if(isset($_GET['usersToVacation']) && isset($_GET['dayId']))
         {
             $_SESSION['calendar'] = json_encode($calend2);
             //czyszczenie Get
-            $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+            // $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
             $_SESSION['Module'] = 1;
-            header("Location: $actual_link");
+            // header("Location: $actual_link");
+            echo '<script>window.history.pushState({}, document.title, "/" + "app/");</script>';
         }
 
 
